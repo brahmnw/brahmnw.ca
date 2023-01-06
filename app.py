9392e399d4b7ec45ad581468a70a21053895affe
Brahm <brahmwongwandanee@gmail.com>
2023-01-06 15:14:29 -0700

    added 404 handling and updated website redesigns

--- app.py
+++ app.py
@@ -7,3 +7,17 @@ app = Flask(__name__)
 def home():
     """ Landing page """
     return flask.render_template('home.html')
+
+@app.route("/about/")
+def about():
+    """ About page """
+    return flask.render_template('about.html')
+
+@app.route("/contact/")
+def contact():
+    """ Contact page """
+    return flask.render_template('contact.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return flask.render_template('errors/404.html'), 404
